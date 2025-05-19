f1ae2f4114c550ff8bcb6422caee7f9a7181d0a0
KC Lim <kclim0528@gmail.com>
2025-05-20 00:13:31 +0800

    built Sider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Sider from './components/Sider'
 
 function App() {
  
   return(
-    <div className='bg-amber-200 h-screen w-screen flex justify-center items-center'>
-      <h1>Hello world</h1>
-    </div>
+    <main className='bg-amber-200 h-screen w-screen flex justify-center items-center'>
+      <Sider/>
+    </main>
   )
 }
 
---
+++ src/components/Sider.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faBuilding,
+  faSpinner,
+  faEnvelope,
+  faFileInvoice,
+  faCalendarAlt,
+  faComments,
+  faCog,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Sider = () => {
+  return (
+    <div className="w-[6vw] h-screen bg-amber-200 flex flex-col">
+      <div className="icons h-[85%] bg-indigo-400 flex flex-col items-center py-8 gap-13 ml-1 mt-1 rounded-r-4xl">
+        <FontAwesomeIcon icon={faHome} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faBuilding} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faSpinner} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faEnvelope} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faFileInvoice} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faCalendarAlt} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faComments} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faCog} className="text-3xl text-white" />
+        <FontAwesomeIcon icon={faUser} className="text-3xl text-white" />
+      </div>
+      <div className="userIcon flex flex-col flex-grow justify-center items-center bg-green-300">
+        {/* import user icon component */}
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Sider;
