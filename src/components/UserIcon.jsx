04fa78a790d743b0a7119bbc079ad6502f58212a
KC Lim <kclim0528@gmail.com>
2025-05-20 01:12:17 +0800

    blueprint of UserIcon done

---
+++ src/components/UserIcon.jsx
@@ -0,0 +1,8 @@
+const UserIcon = () => {
+  return (
+    <div className="userIcon flex flex-col flex-grow justify-center items-center bg-green-300">
+      <img></img>
+    </div>
+  );
+};
+export default UserIcon;
