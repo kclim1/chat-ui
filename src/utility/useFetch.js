02016ad7357178fd0b41a29e2d15e74360b52c22
KC Lim <kclim0528@gmail.com>
2025-05-20 01:12:35 +0800

    built a generic fetch function for all get requests

---
+++ src/utility/useFetch.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const BASE_URL = import.meta.env.VITE_GET_USER_DETAILS;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(`${BASE_URL}${endpoint}`);
+        setData(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [endpoint]);
+
+  return { data, loading, error };
+};
+export default useFetch;
