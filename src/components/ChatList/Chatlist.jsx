3fdd0e7a1513ca0e5e3da3f7f2d676fb37c05294
KC Lim <kclim0528@gmail.com>
2025-05-20 21:12:29 +0800

    removed searchbar

--- src/components/ChatList/Chatlist.jsx
+++ src/components/ChatList/Chatlist.jsx
@@ -3,12 +3,11 @@ import Userlist from "./Userlist";
 
 const Chatlist = () => {
   return (
-    <div className="list-container bg-red-300 w-[350px] h-[400px] flex flex-col justify-around p-4">
-      <Searchbar />
+    <div className="list-container bg-white w-[350px]  h-[450px] flex flex-col justify-around p-4 rounded-lg">
       <Userlist />
 
       {/* import userlist here */}
-      <div className="button-container bg-green-300 flex justify-around gap-4 py-4">
+      <div className="button-container bg-white flex justify-around gap-4 ">
         <button className="bg-indigo-500 text-white text-lg px-4  w-[50%] h-[50px] rounded-lg flex-grow text-center">
           Meeting
         </button>
