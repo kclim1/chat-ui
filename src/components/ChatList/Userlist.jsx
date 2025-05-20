43e1af38b9c9d8e7211b3b060cb1871e90ac8545
KC Lim <kclim0528@gmail.com>
2025-05-20 21:13:02 +0800

    imlpemented search function and fprevented the container from changing sizes when list size changes

--- src/components/ChatList/Userlist.jsx
+++ src/components/ChatList/Userlist.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Userlist = () => {
-  const [userList, setUserList] = useState(null);
+  const [userList, setUserList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     const fetchUserList = async () => {
@@ -23,20 +26,47 @@ const Userlist = () => {
     fetchUserList();
   }, []);
 
+  const filteredUsers = userList.filter((user) =>
+    user.username.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   if (isLoading) return <div>Loading user list...</div>;
   if (error) return <div>Error: {error}</div>;
   if (userList) console.log("userlist is here", userList);
   return (
-    <div className="flex flex-col gap-2 py-2">
-      {userList && userList.length > 0 ? (
-        userList.map((user) => (
-          <div key={user.id} className=" p-2 ">
-            {user.username}
-          </div>
-        ))
-      ) : (
-        <p className="text-gray-500">No users found.</p>
-      )}
+    <div className="container  max-h-[400px] overflow-y-auto flex flex-col">
+      <div className="flex items-center bg-gray-100 text-gray-600 px-3 py-2 rounded-lg w-full">
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="text-gray-500 text-lg mr-2"
+        />
+        <input
+          type="text"
+          placeholder="Search Contact"
+          className="bg-transparent outline-none text-gray-700 flex-grow"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-2 py-2 min-h-[350px]">
+        {filteredUsers && filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => (
+            <div key={user.id} className=" p-2 flex">
+              <img
+                src={user.profileImage}
+                className="w-10 h-10 rounded-full object-cover mr-3"
+              />
+              <div>
+                <p>{user.username}</p>
+                <p>Last message</p>
+                <p>time stamp</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-gray-500">No users found.</p>
+        )}
+      </div>
     </div>
   );
 };
