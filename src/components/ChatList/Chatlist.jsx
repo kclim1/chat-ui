2e6052a8a97935d09a201de81a8af9645028da32
KC Lim <kclim0528@gmail.com>
2025-05-20 15:18:21 +0800

    partially done chatlist

--- src/components/ChatList/Chatlist.jsx
+++ src/components/ChatList/Chatlist.jsx
@@ -1,13 +1,23 @@
+import Searchbar from "./Searchbar";
+import Userlist from "./Userlist";
+
 const Chatlist = () => {
   return (
-    <div className="list-container">
-      {/* search bar  */}
+    <div className="list-container bg-red-300 w-[350px] h-[600px] flex flex-col justify-around p-4">
+      <Searchbar />
+      <Userlist />
+
       {/* userlist here */}
-      <div className="button-container">
-        <button>Meeting</button>
-        <button>Schedule</button>
+      <div className="button-container bg-green-300 flex justify-around gap-4 py-4">
+        <button className="bg-indigo-500 text-white text-lg px-4  w-[50%] h-[50px] rounded-lg flex-grow text-center">
+          Meeting
+        </button>
+        <button className="bg-slate-200 text-black text-lg px-4  w-[50%] h-[50px] rounded-lg flex-grow text-center">
+          Schedule
+        </button>
       </div>
     </div>
   );
 };
+
 export default Chatlist;
