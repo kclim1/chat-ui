c7562308e50f8f57b206603c20ee4ff9b7b0a444
KC Lim <kclim0528@gmail.com>
2025-05-20 16:42:08 +0800

    updated background color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="head ml-4 mt-3 w-[92vw] h-[5vh] bg-pink-200 flex justify-between">
+    <header className="head ml-4 mt-3 w-[92vw] h-[5vh] bg-slate-100 flex justify-between">
       <h2 className="text-2xl font-semibold">Chat</h2>
       <h2 className="text-xl text-indigo-600 underline cursor-pointer hover:text-indigo-800">
         Add New Profile
