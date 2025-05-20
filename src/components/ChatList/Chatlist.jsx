17d2797f3f3f39ffd9692fcdcebcff90c699ad99
KC Lim <kclim0528@gmail.com>
2025-05-20 16:41:28 +0800

    updated chatlist to import userlist

--- src/components/ChatList/Chatlist.jsx
+++ src/components/ChatList/Chatlist.jsx
@@ -3,11 +3,11 @@ import Userlist from "./Userlist";
 
 const Chatlist = () => {
   return (
-    <div className="list-container bg-red-300 w-[350px] h-[600px] flex flex-col justify-around p-4">
+    <div className="list-container bg-red-300 w-[350px] h-[400px] flex flex-col justify-around p-4">
       <Searchbar />
       <Userlist />
 
-      {/* userlist here */}
+      {/* import userlist here */}
       <div className="button-container bg-green-300 flex justify-around gap-4 py-4">
         <button className="bg-indigo-500 text-white text-lg px-4  w-[50%] h-[50px] rounded-lg flex-grow text-center">
           Meeting
