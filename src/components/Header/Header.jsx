02625e22fe2275e5c65fc0a91c60e4ce08663584
KC Lim <kclim0528@gmail.com>
2025-05-20 13:04:04 +0800

    setup header and imported for use in app.js

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="head ml-4 mt-3 w-[92vw] h-[5vh] bg-pink-200 flex justify-between">
+      <h2 className="text-2xl font-semibold">Chat</h2>
+      <h2 className="text-xl text-indigo-600 underline cursor-pointer hover:text-indigo-800">
+        Add New Profile
+      </h2>
+    </header>
+  );
+};
+
+export default Header;
