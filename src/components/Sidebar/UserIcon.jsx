778e14412a1be27ee739a091b70177e7e61aaf5e
KC Lim <kclim0528@gmail.com>
2025-05-20 13:04:30 +0800

    setup user icon to display user profile picture and first name

---
+++ src/components/Sidebar/UserIcon.jsx
@@ -0,0 +1,45 @@
+// src/components/UserIcon.jsx
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserIcon = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(import.meta.env.VITE_GET_CURRENT_USER); //assume you are user #5
+        // console.log("Fetched User Data:", response.data);
+        setUser(response.data);
+      } catch (err) {
+        setError(err.message);
+        console.error("Error Fetching User:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (loading) return <div>Loading user...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className="flex flex-col flex-grow justify-center items-center gap-2 bg-indigo-200">
+      <img
+        src={user?.profileImage || "https://i.pravatar.cc/150?u=default"}
+        alt="User Profile"
+        className="w-12 h-12 rounded-full object-cover"
+      />
+      <h3 className="font-semibold text-sm text-center">
+        {user?.username.split(" ")[0]}
+      </h3>
+    </div>
+  );
+};
+
+export default UserIcon;
