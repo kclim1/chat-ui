ff955f8f0c7ee68765871ed366ea3f29eac57eb5
KC Lim <kclim0528@gmail.com>
2025-05-21 15:49:23 +0800

    stores user id

--- src/store/useUserIdStore.js
+++ src/store/useUserIdStore.js
@@ -3,6 +3,9 @@ import { create } from "zustand";
 const useUserIdStore = create((set) => ({
   selectedUserId: 1,
   setSelectedUserId: (id) => set({ selectedUserId: id }),
+
+  friendProfile: null,
+  setFriendProfile: (data) => set({ friendProfile: data }),
 }));
 
 export default useUserIdStore;
