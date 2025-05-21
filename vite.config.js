8c15bd152c0a4cdb0172def9d789afcb5139b741
KC Lim <kclim0528@gmail.com>
2025-05-21 15:35:03 +0800

    updated vite to bypass cors error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://18.143.79.95",
+        changeOrigin: true,
+        rewrite: (path) => path, // ✅ keep full path
+      },
+    },
+  },
+});
